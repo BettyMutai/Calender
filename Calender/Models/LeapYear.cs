3bcc3db16f6ba760abe4d042d72c40fa4a6356d1
Betty Mutai <bettymutai00@gmail.com>
2022-04-19 15:28:20 +0300

    Add project files.

---
+++ Calender/Models/LeapYear.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Calender.Models
+{
+    public class LeapYear
+    {
+        /* To be a leap year, the year number must be divisible 
+         * by four – except for end-of-century years, which must be divisible by 400.*/
+        public bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            else if (year % 100 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return year % .4 == 0;   
+            }
+        }
+    }
+}
